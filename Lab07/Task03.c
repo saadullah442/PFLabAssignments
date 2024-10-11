8081f96ff2297266faa64d67929546c8bc6e590d
Saad Ullah <83773413+saadullah442@users.noreply.github.com>
2024-10-11 18:17:09 +0500

    Add files via upload

--- Lab07/Task03.c
+++ Lab07/Task03.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
 int main() {
-
+  // Declaring variables
   char name[20];
-  char email[60];
-  char number[30];
+  char email[30];
+  char phone[20];
 
+  // validating all inputs
+  printf("Enter name: ");
+  scanf(" %[a-zA-z]", name);
+  
 
-  printf("Enter Name: ");
-  scanf(" %[ A-Za-z]", name);
+  printf("Enter email: ");
+  scanf(" %[0-9a-zA-Z@a-zA-Z0-9.]", email);
+ 
 
-  /*printf("Enter email: ");
-  scanf(" %[%@%]", email);*/
+  printf("Enter phone: ");
+  scanf(" %[+0-9- ]", phone);
+  
 
-  printf("Enter Phone number: ");
-  scanf(" %[ +0-9%-]", number);
-
-  printf("%s\n", name);
-  printf("%s\n", email);
-  printf("%s\n", number);
-
-  return 0;
+  // Printing all the validated inputs
+  printf("Your name is: %s\n", name);
+  printf("Your email is: %s\n", email);
+  printf("Your phone number is: %s\n", phone);
+  
 }
