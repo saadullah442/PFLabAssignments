0dc6b62bbd0d32c245c403bc32b50a8edfe0f0a8
Saad Ullah <83773413+saadullah442@users.noreply.github.com>
2024-10-11 11:07:08 +0500

    Add files via upload

--- Lab07/Task6.c
+++ Lab07/Task6.c
@@ -1,33 +1,52 @@
 #include <stdio.h>
 int main() {
-
+// Declaring variables
   int num,size;
   int i,j;
   i= 0;
   j = 0;
+
+// Asking for array size
   printf("Enter array size: ");
   scanf("%d", &size);
  
   int arr[size];
 
+// Asking for numbers and inserting them to array
   for(i=0;i < size; i++){
     printf("Enter number: ");
     scanf("%d", &num);
-    arr[i] = num;
-      
+    arr[i] = num;   
   }
 
+/* first loop to find duplicate element.
+   This loop will pick one element*/
+i=0;
 firstloop:
   do{
-    i++;
-    goto secondloop;
+  
+    if (i != (size - 1)) goto secondloop;
+  
+  }while(i < (size - 1));
 
-  }while(i < size);
+/* This statement will only run when firstloop is finished
+   ending the program */
+return 0;
 
+/* second loop to find duplicate element.
+   This loop will check if the picked element exists in different index or not */
 secondloop:
- j = 0;
+ j = i + 1;
  do{
-   if(arr[i] == arr[j]) printf("%d is repeated in array\n", arr[i]);
+   
+   
+   if(arr[i] == arr[j]) {
+     printf("%d is repeated in array\n", arr[i]);
+     i++;
+     goto firstloop;
+   }
+   j++;
  }while(j < size);
+ i++;
  goto firstloop;
 }
