4b034353baf62aedecb1caae89a90fa4f79ae310
Saad Ullah <83773413+saadullah442@users.noreply.github.com>
2024-10-23 13:17:17 +0500

    Add files via upload

--- Lab08/Task08.c
+++ Lab08/Task08.c
@@ -5,8 +5,8 @@ int main(){
 	int i,j;
 	int matrix[][size];
 	int sdl_pt = 999999;
-	int sdl_i;
-	int sdl_j;
+	int sdl_i = 0;
+	int sdl_j = 0;
 	
 	// Taking numbers as input
 	for(i=0;i<num;i++){
@@ -15,15 +15,31 @@ int main(){
 			scanf("%d", &matrix[i][j]);
 		}
 	}
-	j=0;
-	// Finding saddle point(s) by row
-	for(i=0;i < size;i++){
-		for(j=0;j<size;j++){
-			if(matrix[i][j] < sdl_pt) {
-				sdl_pt = matrix[i][j];
-				sdl_j = j;
+
+	
+	// Finding saddle point(s)
+	i=0;j=0;
+	while(i < size && j < size){
+		// Finding saddle point(s) by row
+	 	for(j=0;j<size;j++){
+			if(matrix[sdl_i][j] < sdl_pt) {
+					sdl_pt = matrix[sdl_i][j];
+					sdl_j = j;
 			}
 		}
+		
+		// Finding saddle point(s) by colum
+		for(;i<size;i++){
+			if(matrix[i][sdl_j] > sdl_pt) {
+					break;
+			}
+		}
+		
+		
+	}
+	
+	for(i=0;i < size;i++){
+		
 	}
 	
 	// Finding saddle point(s) by colum;
@@ -35,12 +51,6 @@ int main(){
 			}
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
+
 	return 0;
 }
