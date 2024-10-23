1c3b7090ccb2a65c7336702a2593761cce679350
Saad Ullah <83773413+saadullah442@users.noreply.github.com>
2024-10-23 22:37:59 +0500

    Add files via upload

--- Lab08/Task08.c
+++ Lab08/Task08.c
@@ -3,14 +3,14 @@
 int main(){
 	int size = 3;
 	int i,j;
-	int matrix[][size];
-	int sdl_pt = 999999;
+	int matrix[size][size];
+	int sdl_pt = 9999999;
 	int sdl_i = 0;
 	int sdl_j = 0;
 	
 	// Taking numbers as input
-	for(i=0;i<num;i++){
-		for(j=0;j<num;j++){
+	for(i=0;i<size;i++){
+		for(j=0;j<size;j++){
 			printf("Enter number: ");
 			scanf("%d", &matrix[i][j]);
 		}
@@ -19,9 +19,10 @@ int main(){
 	
 	// Finding saddle point(s)
 	i=0;j=0;
-	while(i < size && j < size){
+	while(sdl_i < size){
 		// Finding saddle point(s) by row
 	 	for(j=0;j<size;j++){
+			// Changing Colum
 			if(matrix[sdl_i][j] < sdl_pt) {
 					sdl_pt = matrix[sdl_i][j];
 					sdl_j = j;
@@ -29,28 +30,20 @@ int main(){
 		}
 		
 		// Finding saddle point(s) by colum
-		for(;i<size;i++){
+		for(i=0;i<size;i++){
+			// Changing row 
 			if(matrix[i][sdl_j] > sdl_pt) {
-					break;
+				
+				sdl_pt = 9999999;	
+				break;
 			}
 		}
-		
-		
+		if (sdl_pt != 9999999) printf("Saddle point is: %d\n", sdl_pt);
+		sdl_pt = 9999999;
+		sdl_i++;		
 	}
 	
-	for(i=0;i < size;i++){
-		
-	}
 	
-	// Finding saddle point(s) by colum;
-	for(i=0;i < size;i++){
-		for(j=0;j<size;j++){
-			if(matrix[i][j] < sdl_pt) {
-				sdl_pt = matrix[i][j];
-				sdl_j = j;
-			}
-		}
-	}
 
 	return 0;
 }
